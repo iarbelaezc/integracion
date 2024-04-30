d1aaef7a1dc0e9e3eef77e2d79b13526f3af09c0
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-04-30 14:54:27 -0500

    Update 1_Internet De Las Cosas.py

--- pages/1_Internet De Las Cosas.py
+++ pages/1_Internet De Las Cosas.py
@@ -1,3 +1,14 @@
 import streamlit as st
-
+from PIL import Image 
 st.title("Internet de las cosas")
+
+st.header("Clase Marzo 10")
+
+st.title(" Aplicación para ciudades inteligentes")
+
+st.header( "En este espacio podrás obtener información de tu ciudad.")
+
+image= Image.open ('inteligencia.jpg')
+st.image(image,caption= 'Inteligencia Urbana')
+st.write("Enlace para mi sistema de internet de las cosas")
+st.write("Ingresa al link [link] (https://demo.thingsboard.io/dashboards/221eb150-db1f-11ee-bc03-55147b89654f)")
