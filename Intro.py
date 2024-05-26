4f77482d877e5c650f938d9a4b99f01d0755eab0
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 17:09:42 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.title("Página inicial")
+st.title("Introduccíón al sistema")
+
+st.header("¿Qué hacemos?")
+
+st.write("Está pagina se especializa en la captación, análisis y procesamiento de datos realcionados con la calidad del aire en Medellín")
+
+st.header("Nuestra visión")
+
+st.write("Por medio de diferentes sistemas como conectar sensores a internet, análisis de datos e inteligencia artificial, buscamos ser una página que pueda contribuir a la educación y concientización ambiental.")
+
