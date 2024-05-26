ba29c98b4ab25c5d304273b616f3f5913de0068a
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 17:37:27 -0500

    Update Identificador visual fuentes de contaminacion.py

--- pages/Identificador visual fuentes de contaminacion.py
+++ pages/Identificador visual fuentes de contaminacion.py
@@ -11,7 +11,7 @@ def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
 
-st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Analisis dde imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
 #st.write(openai.Model.list())
