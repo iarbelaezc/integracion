d2f868a69efe1cd58d5cd54fec61b8fa284091cc
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 17:39:03 -0500

    Update Identificador visual fuentes de contaminacion.py

--- pages/Identificador visual fuentes de contaminacion.py
+++ pages/Identificador visual fuentes de contaminacion.py
@@ -20,13 +20,12 @@ ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
 
-# Retrieve the OpenAI API Key from secrets
-api_key = os.environ['OPENAI_API_KEY']
+st.header("Encuentra fuentes de contaminación")
+st.write("Ingresa imágenes para identificar fuentes de contaminación")
 
 # Initialize the OpenAI client with the API key
 client = OpenAI(api_key=api_key)
-st.header("Encuentra fuentes de contaminación")
-st.write("Ingresa imágenes para identificar fuentes de contaminación")
+
 # File uploader allows user to add their own image
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
