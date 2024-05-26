cda2ff11fd4f93c948667af073ed2a0e7d095524
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 17:14:17 -0500

    Update and rename 1_Internet De Las Cosas.py to Información metereológica.py

--- pages/Información metereológica.py
+++ pages/Información metereológica.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from PIL import Image 
-st.title("Internet de las cosas")
+st.title("Mapa de sensores de calidad del aire")
 
-st.header("Clase Marzo 10")
+st.header("Este sistema se construyó con un sistema de sensores conetados a internet (IoT) ")
 
-st.title(" Aplicación para ciudades inteligentes")
+st.title(" ¿En qué influye el clima con la calidad del aire?")
 
 st.header( "En este espacio podrás obtener información de tu ciudad.")
 
 image= Image.open ('inteligencia.jpg')
 st.image(image,caption= 'Inteligencia Urbana')
-st.write("Enlace para mi sistema de internet de las cosas")
+st.write("Enlace para acceder sensores climáticos en tiempo real")
 st.write("Ingresa al link [link] (https://demo.thingsboard.io/dashboards/221eb150-db1f-11ee-bc03-55147b89654f)")
