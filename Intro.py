223c03ea6be1c8fc5aecf9a06182533de3f76fd9
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 18:09:24 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -9,3 +9,13 @@ st.header("Nuestra visión")
 
 st.write("Por medio de diferentes sistemas como conectar sensores a internet, análisis de datos e inteligencia artificial, buscamos ser una página que pueda contribuir a la educación y concientización ambiental.")
 
+st.header("¿Qué es la calidad del aire?")
+
+st.write("La calidad del aire se refiere a la medida de contaminantes presentes en el aire que respiramos, y cómo estos contaminantes pueden afectar la salud humana, animal y el medio ambiente en general. Estos contaminantes pueden incluir gases, partículas sólidas o líquidas en suspensión, compuestos químicos, entre otros.")
+
+st.write("La calidad del aire se evalúa mediante la medición de la concentración de diversos contaminantes en el aire, como el dióxido de carbono (CO2), el dióxido de azufre (SO2), el monóxido de carbono (CO), los óxidos de nitrógeno (NOx), el ozono (O3), las partículas en suspensión (PM10, PM2.5), entre otros. Estos contaminantes pueden provenir de diversas fuentes, como la quema de combustibles fósiles, la industria, el transporte, la agricultura, entre otros.")
+
+st.write("La calidad del aire es importante para la salud humana, ya que la inhalación de aire contaminado puede causar una serie de problemas respiratorios y de salud, como enfermedades cardiovasculares, problemas respiratorios crónicos, exacerbación de alergias, asma, entre otros. Además, la contaminación del aire también puede afectar la vegetación, los ecosistemas acuáticos y terrestres, y contribuir al cambio climático.")
+
+st.write("Por lo tanto, monitorear y mejorar la calidad del aire es crucial para proteger la salud humana y el medio ambiente, y se lleva a cabo a través de la implementación de políticas y regulaciones ambientales, así como de programas de monitoreo y control de la contaminación del aire.")
+
