5eb940a75a54b747a900799cfbc71de9e672ed51
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-05-26 20:22:33 -0500

    Update and rename 4. Realidades extendidas to 4. Realidades extendidas.py

---
+++ pages/4. Realidades extendidas.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Exposición virtual")
+st.write("Te invitamos a visitar nuestra exposición por medio de realidad aumentada acerca de calidad del aire")
+st.write("Enlace para acceder a la exposición")
+st.write("Ingresa al link [link] ()")
+st.write("Nota: Todas las imágenes fueron realizadas conla ayuda de la inteligencia artificial Midjourney. No asumimos propiedad de ninguna de las obras presentes, y damos crédito a todos los respectivos autores.")
